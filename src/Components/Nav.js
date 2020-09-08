263d17cdec60079cdb4c482502481ef726eff688
Calvin Kuhnen <ckuhnen@hawk.iit.edu>
2020-09-08 03:40:15 -0500

    setup nav, added css and fonts from old site

---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../style_home.css';
+
+const Nav = () => {
+  return (
+    <nav>
+      <div id="title">
+        Calvin Kuhnen
+      </div>
+      <div id="navbar">
+        <a href="./projects">Projects</a>
+        <a href="./resume">Resume</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
