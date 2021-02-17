92799e4e95808b7c10d5d69acb68256aa4795ce9
Calvin Kuhnen <ckuhnen@hawk.iit.edu>
2021-02-17 01:38:41 -0600

    started to make project pages mobile friendly

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
           <div className="horizontal-container">
             <div id="about">
         			<h3>About Me</h3>
-        			<p>Hi! I'm a recent graduate of Illinois Institute of Technology, with a major in computer science and a passion for full stack development. I also like developing platforms for media and have a growing interest in automation and AI, both of which I hope to expand on in my future.</p>
+        			<p>Hi! I'm a recent graduate of Illinois Institute of Technology, with a major in computer science. I like developing platforms for media and have a growing interest in automation and AI, both of which I hope to expand on in my future.</p>
               <h3>Contact Info</h3>
               <div><a className="contact-link" href="mailto:calvinkuhnen@gmail.com" rel="noopener noreferrer" target="_blank">calvinkuhnen@gmail.com</a></div>
               <div><a className="contact-link" href="https://github.com/cal-kuhnen" rel="noopener noreferrer" target="_blank">GitHub</a></div>
