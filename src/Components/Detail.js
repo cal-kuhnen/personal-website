42b43553337563acd67b919cc35a430bb7a3065c
Calvin Kuhnen <calvinkuhnen@gmail.com>
2021-03-08 01:51:21 -0600

    changed to switch statement for rendering elements

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -10,22 +10,16 @@ const Detail = () => {
 
   var choice = portfolio.find(item => item.id === project);
 
-  if (choice.style === 'dual') {
-    return (
-      <div className='container'>
-        <DualColumn proj={choice} />
-      </div>
-    )
-  }
-  else {
-    return (
-      <SingleColumn proj={choice} />
-    )
-  }
+
+  return (
+    <div className='container'>
+      <Elements proj={choice} />
+    </div>
+  )
 }
 
-// Function to render page in 2-column style
-const DualColumn = (item) => {
+// Function to render content in different styles based on json doc property 'type'
+const Elements = (item) => {
 
   document.documentElement.style.setProperty('--backing', item.proj.colors.backing);
   document.documentElement.style.setProperty('--button', item.proj.colors.button);
@@ -34,168 +28,146 @@ const DualColumn = (item) => {
 
   let dualStyle = item.proj.detail.map((project) => {
 
-    if (project.type === 'left') {
-      return (
-        <div key={project.id} className='horizontal-container justify-between'>
-          <div className='column'>
-            <div className='left-right'>
-              <div dangerouslySetInnerHTML={ {__html: project.caption} } />
+    switch (project.type) {
+      case 'left':
+        return (
+          <div key={project.id} className='horizontal-container justify-between'>
+            <div className='column'>
+              <div className='left-right'>
+                <div dangerouslySetInnerHTML={ {__html: project.caption} } />
+              </div>
+            </div>
+            <div className='column'>
+              <div className='left-right'>
+                <img className='double' src={project.image} alt={project.alt}></img>
+              </div>
             </div>
           </div>
-          <div className='column'>
-            <div className='left-right'>
-              <img className='double' src={project.image} alt={project.alt}></img>
-            </div>
-          </div>
-        </div>
-      )
-    }
-    else if (project.type === 'title') {
-      return (
-        <h2 key={project.id} className='subtitle'>{project.title}</h2>
-      )
-    }
-    else if (project.type === 'right') {
-      return (
-        <div key={project.id} className='horizontal-container justify-between'>
-          <div className='left left-right'>
-            <img className='dual-image' src={project.image} alt={project.alt}></img>
-          </div>
-          <div className='left-right'>
-            {project.caption}
-          </div>
-        </div>
-      )
-    }
-    else if (project.type === 'doubleImg') {
-      return (
-        <div key={project.id} className='horizontal-container justify-between'>
-          <div className='column'>
-            <div className='left-right backing center'>
-              <img className={project.class} src={project.left} alt={project.leftAlt}></img>
-            </div>
-            <div className='caption'>
-              {project.caption1}
-            </div>
-          </div>
-          <div className='column'>
-            <div className='left-right backing center'>
-              <img className={project.class} src={project.right} alt={project.rightAlt}></img>
-            </div>
-            <div className='caption'>
-              {project.caption2}
-            </div>
-          </div>
-        </div>
-      )
-    }
-    else if (project.type === 'code') {
-      return (
-        <div key={project.id} className='offset-container'>
-          <SyntaxHighlighter language="assembly" className="code">
-            {project.codeSnippet}
-          </SyntaxHighlighter>
-          <div className='center-caption'>
-            {project.caption}
-          </div>
-        </div>
-      )
-    }
-    else if (project.type === 'singleImg') {
-      return (
-        <div key={project.id} className='offset-container'>
-          <div className='vertical-container'>
-            <div className='left-right backing'>
-              <img className={project.class} src={project.image} alt={project.alt}></img>
-            </div>
-          </div>
-          <div className='center-caption'>
-            {project.caption}
-          </div>
-        </div>
-      )
-    }
-    else if (project.type === 'video') {
-      return (
-        <div key={project.id} className='offset-container'>
-          <div className='vertical-container'>
-            <div className='left-right'>
-              <video src={project.video} alt={project.alt} controls={project.controls} autoPlay={project.autoplay} loop={project.loop} muted={project.muted}></video>
-            </div>
-          </div>
-          <div className='center-caption'>
-            {project.caption}
-          </div>
-        </div>
-      )
-    }
-    else if (project.type === 'doubleVid') {
-      return (
-        <div key={project.id} className='horizontal-container justify-between'>
-          <div className='column'>
-            <div className='left-right'>
-              <video className='double' src={project.left} alt={project.leftAlt} controls={project.controls} autoPlay={project.autoplay} loop={project.loop} muted={project.muted}></video>
-            </div>
-            <div className='caption'>
-              {project.caption1}
-            </div>
-          </div>
-          <div className='column'>
-            <div className='left-right'>
-              <video className='double' src={project.right} alt={project.rightAlt} controls={project.controls} autoPlay={project.autoplay} loop={project.loop} muted={project.muted}></video>
-            </div>
-            <div className='caption'>
-              {project.caption2}
-            </div>
-          </div>
-        </div>
-      )
-    }
+        )
+        break;
 
-    return (
-      <div>
-       Bad object.
-      </div>
-    )
+      case 'title':
+        return (
+          <h2 key={project.id} className='subtitle'>{project.title}</h2>
+        )
+        break;
+
+      case 'right':
+        return (
+          <div key={project.id} className='horizontal-container justify-between'>
+            <div className='left left-right'>
+              <img className='dual-image' src={project.image} alt={project.alt}></img>
+            </div>
+            <div className='left-right'>
+              {project.caption}
+            </div>
+          </div>
+        )
+        break;
+
+      case 'doubleImg':
+        return (
+          <div key={project.id} className='horizontal-container justify-between'>
+            <div className='column'>
+              <div className='left-right backing center'>
+                <img className={project.class} src={project.left} alt={project.leftAlt}></img>
+              </div>
+              <div className='caption'>
+                {project.caption1}
+              </div>
+            </div>
+            <div className='column'>
+              <div className='left-right backing center'>
+                <img className={project.class} src={project.right} alt={project.rightAlt}></img>
+              </div>
+              <div className='caption'>
+                {project.caption2}
+              </div>
+            </div>
+          </div>
+        )
+        break;
+
+      case 'code':
+        return (
+          <div key={project.id} className='offset-container'>
+            <SyntaxHighlighter language="assembly" className="code">
+              {project.codeSnippet}
+            </SyntaxHighlighter>
+            <div className='center-caption'>
+              {project.caption}
+            </div>
+          </div>
+        )
+        break;
+
+      case 'singleImg':
+        return (
+          <div key={project.id} className='offset-container'>
+            <div className='vertical-container'>
+              <div className='left-right backing'>
+                <img className={project.class} src={project.image} alt={project.alt}></img>
+              </div>
+            </div>
+            <div className='center-caption'>
+              {project.caption}
+            </div>
+          </div>
+        )
+        break;
+
+      case 'video':
+        return (
+          <div key={project.id} className='offset-container'>
+            <div className='vertical-container'>
+              <div className='left-right'>
+                <video src={project.video} alt={project.alt} controls={project.controls} autoPlay={project.autoplay} loop={project.loop} muted={project.muted}></video>
+              </div>
+            </div>
+            <div className='center-caption'>
+              {project.caption}
+            </div>
+          </div>
+        )
+        break;
+
+      case 'doubleVid':
+        return (
+          <div key={project.id} className='horizontal-container justify-between'>
+            <div className='column'>
+              <div className='left-right'>
+                <video className='double' src={project.left} alt={project.leftAlt} controls={project.controls} autoPlay={project.autoplay} loop={project.loop} muted={project.muted}></video>
+              </div>
+              <div className='caption'>
+                {project.caption1}
+              </div>
+            </div>
+            <div className='column'>
+              <div className='left-right'>
+                <video className='double' src={project.right} alt={project.rightAlt} controls={project.controls} autoPlay={project.autoplay} loop={project.loop} muted={project.muted}></video>
+              </div>
+              <div className='caption'>
+                {project.caption2}
+              </div>
+            </div>
+          </div>
+        )
+        break;
+
+      default:
+        return (
+          <div>
+           Bad object.
+          </div>
+        )
+      }
     })
 
-  if (item.proj.hasOwnProperty('link')) {
-    return (
-      <div className='dual-container'>
-        <h1 className='top'><a className='project-link' rel="noopener noreferrer" target="_blank" href={item.proj.link}>{item.proj.title}</a></h1>
-        {dualStyle}
-      </div>
-    )
-  }
-  else {
-    return (
-      <div className='dual-container'>
-        <h1 className='top'>{item.proj.title}</h1>
-        {dualStyle}
-      </div>
-    )
-  }
-}
-
-const SingleColumn = (item) => {
-
-  let singleStyle = item.proj.detail.map((project) => {
-    return (
-      <div className='vertical-container'>
-        <div className='center'>
-          <img className='single-image' src={project.image} alt={project.alt}></img>
-        </div>
-        <div className='text'>
-          {project.caption}
-        </div>
-      </div>
-    )
-  })
 
   return (
-    <div className='single-container'>
+    <div className='dual-container'>
       <h1 className='top'>{item.proj.title}</h1>
-      {singleStyle}
+      {dualStyle}
     </div>
   )
 }
