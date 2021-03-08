eb7dcc0d45ee81c3d43f1f8b844c6c9e78cc7904
Calvin Kuhnen <calvinkuhnen@gmail.com>
2021-03-08 02:37:49 -0600

    updated resume and navbar styles

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -11,47 +11,54 @@ const Resume = () => {
     <div className="container">
   		<section id="resume">
         <div className='res-container'>
-    			<section>
+    			<section className="chunk">
     				<h3>Education</h3>
-            <div className="org-info">
-              <div className="left-col">
-      					<h4 className="org">Illinois Institute of Technology</h4>
-                <p className="indent">Bachelor of Science in Computer Science</p>
+            <div className="piece">
+              <div className="org-info">
+                <div className="left-col">
+        					<h4 className="org">Illinois Institute of Technology</h4>
+                  <p className="indent">Bachelor of Science in Computer Science</p>
+                </div>
+                <div className="right-col">
+        					<p className="date-loc">Chicago, Illinois</p>
+                  <p className="date-loc">August 2018 to December 2020</p>
+                </div>
               </div>
-              <div className="right-col">
-      					<p className="date-loc">Chicago, Illinois</p>
-                <p className="date-loc">August 2018 to December 2020</p>
+      				<ul>
+      					<li>Relevant Courses: Calculus | Discrete Structures | Systems Programming |
+    Database Organization | Intro to Algorithms | Operating Systems | Software Engineering | Intro to Artificial Intelligence | Mobile App Design | Intro to Information Security</li>
+      					<li>GPA: 3.7/4.0</li>
+      				</ul>
+            </div>
+            <div className="piece">
+              <div className="org-info">
+                <div className="left-col">
+                  <h4 className="org">Oakton Community College</h4>
+        					<p className="indent">Computer Science</p>
+                </div>
+                <div className="right-col">
+                  <p className="date-loc">Des Plaines, Illinois</p>
+                  <p className="date-loc">June 2017 to July 2018</p>
+                </div>
               </div>
             </div>
-    				<ul>
-    					<li>Relevant Courses: Calculus | Discrete Structures | Systems Programming |
-  Database Organization | Intro to Algorithms | Operating Systems | Software Engineering | Intro to Artificial Intelligence | Mobile App Design | Intro to Information Security</li>
-    					<li>GPA: 3.7/4.0</li>
-    				</ul>
-            <div className="org-info">
-              <div className="left-col">
-                <h4 className="org">Oakton Community College</h4>
-      					<p className="indent">Computer Science</p>
-              </div>
-              <div className="right-col">
-                <p className="date-loc">Des Plaines, Illinois</p>
-                <p className="date-loc">June 2017 to July 2018</p>
-              </div>
-            </div>
-            <div className="org-info">
-              <div className="left-col">
-      					<h4 className="org">The Cooper Union</h4>
-                <p className="indent">Mechanical Engineering</p>
-              </div>
-              <div className="right-col">
-                <p className="date-loc">New York, New York</p>
-                <p className="date-loc">August 2015 to December 2016</p>
+            <div className="piece">
+              <div className="org-info">
+                <div className="left-col">
+        					<h4 className="org">The Cooper Union</h4>
+                  <p className="indent">Mechanical Engineering</p>
+                </div>
+                <div className="right-col">
+                  <p className="date-loc">New York, New York</p>
+                  <p className="date-loc">August 2015 to December 2016</p>
+                </div>
               </div>
             </div>
     			</section>
 
-    			<section id="experience">
+    			<section className="chunk">
     				<h3>Experience</h3>
+            <div className="piece">
               <div className="org-info">
       					<div className="left-col">
                   <h4 className="org">EMP Data Center Corporation</h4>
@@ -62,12 +69,14 @@ const Resume = () => {
                   <p className="date-loc">Feb 2017 to Jul 2018</p>
                 </div>
               </div>
-    						<ul>
-    							<li>Managed maintenance and upkeep of client server systems</li>
-    							<li>Provided hands-on assistance for clients</li>
-    							<li>Maintained weekly reports for security, maintenance, and other systems</li>
-    							<li>As the senior Operations Associate, trained new hires and performed extra duties in the Network Operations Center</li>
-    						</ul>
+    					<ul>
+    						<li>Managed maintenance and upkeep of client server systems</li>
+    						<li>Provided hands-on assistance for clients</li>
+    						<li>Maintained weekly reports for security, maintenance, and other systems</li>
+    						<li>As the senior Operations Associate, trained new hires and performed extra duties in the Network Operations Center</li>
+    					</ul>
+            </div>
+            <div className="piece">
               <div className="org-info">
                 <div className="left-col">
         					<h4 className="org">Cooper Union FSAE Team</h4>
@@ -78,14 +87,15 @@ const Resume = () => {
                   <p>August 2015 to December 2016</p>
                 </div>
               </div>
-    						<ul>
-    							<li>Led a team of 4 in designing steering geometry for a student-built race car</li>
-    							<li>Primary contributor to CAD modeling</li>
-    							<li>Worked closely with other subsystems to organize designs</li>
-    							<li>Acted as point of communication between overall team leaders and my team</li>
-    						</ul>
+    					<ul>
+    						<li>Led a team of 4 in designing steering geometry for a student-built race car</li>
+    						<li>Primary contributor to CAD modeling</li>
+    						<li>Worked closely with other subsystems to organize designs</li>
+    						<li>Acted as point of communication between overall team leaders and my team</li>
+    					</ul>
+            </div>
     			</section>
-    			<section id="skills">
+    			<section className="chunk">
     				<h3>Skills & Interests</h3>
     					<h4 className="interests">Programming Languages:</h4>
     						<p className="list">Python, Javascript, Java, C, C++, SQL, R</p>
